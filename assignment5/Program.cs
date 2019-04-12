dddf6a2718434f1531fef05d18cff4271f2f33d2
visualstudio <visualstudio@122-PC02>
2019-04-12 18:24:24 -0400

    first working code

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -19,4 +19,35 @@ namespace assignment5
             Console.WriteLine(orange.Gematria("universe"));
             Console.ReadLine();
         }
-       
+        public int Gematria(string word)
+        {
+            int GematriaValue = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                GematriaValue += LetterValue(word[i].ToString());
+            }
+            return GematriaValue;
+        }
+        public int LetterValue(string letter)
+        {
+
+            int x = 0;
+
+            while (Letters[x] != letter)
+            {
+
+
+                if (Letters[x++] == letter)
+                    return -1;
+            }
+
+
+            return x + 1;
+        }
+
+
+    }
+
+
+}
